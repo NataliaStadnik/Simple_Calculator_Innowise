c9d9958ed74ae7061848c9dbbebb7fbf96288215
NataliaStadnik <128145143+NataliaStadnik@users.noreply.github.com>
2025-01-11 13:09:48 +0300

    fixed jest settings

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,4 @@ module.exports = {
   transform: {
     '\\.js$': 'babel-jest',
   },
-  moduleNameMapper: {
-    '\\.(css)$': '<rootDir>/__mocks__/styleMock.js',
-  },
-
-  testEnvironment: 'jsdom',
 };
